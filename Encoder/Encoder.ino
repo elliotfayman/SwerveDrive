a8cfc4ab02c551a0a94d585a03652756da3df63e
elliotfayman <elliotfayman@gmail.com>
2023-03-24 21:56:00 -0700

    Fixing the encoder code. Allowing for accurate detection of forward and back motion

--- Encoder/Encoder.ino
+++ Encoder/Encoder.ino
@@ -2,11 +2,14 @@ const int encoder_pin1 = 2;
 const int encoder_pin2 = 3;
 volatile int count = 0;
 float position = 0.0;
+bool lastA;
 
 void setup() {
-  pinMode(encoder_pin1, INPUT);
-  pinMode(encoder_pin2, INPUT);
+  pinMode(encoder_pin1, INPUT_PULLUP);
+  pinMode(encoder_pin2, INPUT_PULLUP);
+  pinMode(9, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(encoder_pin1), encoder_callback, CHANGE);
+  lastA = digitalRead(encoder_pin1);
   Serial.begin(9600);
 }
 
@@ -14,14 +17,21 @@ void loop() {
   Serial.print("Motor position: ");
   Serial.print(position);
   Serial.println(" degrees");
+  digitalWrite(9, HIGH);
   delay(100);
 }
 
 void encoder_callback() {
-  if (digitalRead(encoder_pin2)) {
-    count++;
-  } else {
-    count--;
+  bool newA = digitalRead(encoder_pin1);
+  bool newB = digitalRead(encoder_pin2);
+
+  if (newA != lastA) {
+    if (newB != lastA) {
+      count++;
+    } else {
+      count--;
+    }
+    lastA = newA;
   }
-  position = count / 25.9 * 360;
+  position = count * 13.89/2;
 }
